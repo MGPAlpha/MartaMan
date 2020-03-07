6b7833ca499474a4c44c500deac15f2eb294b5da
Mitchell Philipp <mgpalpha1@gmail.com>
2020-03-07 12:10:58 -0500

    create a class for storing game data

--- bot.js
+++ bot.js
@@ -4,6 +4,95 @@ var Twit = require('twit');
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
 
+var phrases = [
+	//TODO replace these with hangman phrases
+	"Phrases go here"
+];
+
+var hangmanArt = [
+	"",
+	"",
+	"",
+	"",
+	""
+];
+
+class Game {
+	constructor() {
+		this._letters = new Map();
+		for (var i = 10; i < 36; i++) {
+			this._letters.set(i.toString(36), false);
+		}
+
+		this._phrase = phrases[Math.floor(Math.random() * phrases.length)];
+
+		this._wrongGuesses = 0;
+
+		// Meaning:
+		// -2: Guess could not be parsed
+		// -1: Letter was already guessed
+		// 0: Incorrect guess
+		// 1: Correct guess
+		// undefined: no previous guess
+		this._lastGuessResult = undefined;
+		
+		// For the win state, 0 represents a game still running, -1 is lost, and 1 is won
+		this._winState = 0;
+	}
+
+	letterGuessed(letter) {
+		if (letter.length != 1) {
+			throw "Cannot parse " + letter + " as a letter";
+		}
+		if (letter.match(/\W/g) != 1) return true;
+		else return this.letters.get(letter);
+	}
+
+	guess(letter) {
+		if (letter.length != 1 || !letter.match(/\w/)) {
+			throw "Cannot guess " + letter + "";
+			this._lastGuessResult = -2;
+		}
+		if (this.letters.get(letter.toLowerCase())) {
+			this._lastGuessResult = -1;
+		} else {
+			this.letters.set(letter.toLowerCase(), true);
+			if (this.phrase.includes(letter)) {
+				this._lastGuessResult = 1;
+				for (var i = 0; i < this.phrase.length; i++) {
+					
+				}
+			} else {
+				this._lastGuessResult = 0;
+				this._wrongGuesses++;
+				if (this.wrongGuesses >= hangmanArt.length) {
+					this._winState = -1;
+				}
+			}
+		}
+	}
+
+	get letters() {
+		return this._letters;
+	}
+
+	get phrase() {
+		return this._phrase;
+	}
+
+	get wrongGuesses() {
+		return this._wrongGuesses;
+	}
+
+	get lastGuessResult() {
+		return this._lastGuessResult
+	}
+
+	get winState() {
+		return this._winState;
+	}
+}
+
 // Remembers the id of MartaMan's user to make sure it doesn't reply to itself over and over
 var myId;
 
@@ -55,6 +144,8 @@ function completedInitTask() {
 	}
 }
 
+var games = new Map();
+
 function update() {
 	// Make sure another update isn't already being run
 	if (!occupied) {
@@ -62,23 +153,36 @@ function update() {
 		// Get all new mentions
 		T.get('statuses/mentions_timeline', {since_id: lastSeenMention}, (error, data) => {
 			if (!error) {
+				
 				// Loop through the list of new mentions
 				data.forEach(element => {
+					let userId = element.user.id_str;
+					let repliedTo = [];
 					// Make sure the mention isn't own tweet
-					if (element.user.id_str != myId) {
-						// Post a reply tweet, with the same text minus the last character
-						var status = '\u25b8' + element.text.replace(/@[\w\d_]* ?/g, "") + '\u25c2';
-						console.log(status);
-						T.post('statuses/update', {
-							status: status,
-							in_reply_to_status_id: element.id_str,
-							auto_populate_reply_metadata: true
-						}, (error, response) => {
-							if (error) {
-								console.log("Error occurred in replying");
-								console.log(error);
-							}
-						});
+					if (userId != myId && !repliedTo.includes(userId)) {
+						
+						if (!games.has(userId)) {
+							// Post a reply tweet, with the same text
+							var status = '\u25b8' + element.text.replace(/@[\w\d_]* ?/g, "") + '\u25c2';
+							console.log(status);
+							T.post('statuses/update', {
+								status: status,
+								in_reply_to_status_id: element.id_str,
+								auto_populate_reply_metadata: true
+							}, (error, response) => {
+								if (error) {
+									console.log("Error occurred in replying");
+									console.log(error);
+								} else {
+									games.set(userId, new Game());
+								}
+							});
+						} else {
+							console.log(games.get(userId));
+						}
+						
+						repliedTo.push(userId);
+
 					}
 				});
 				// Update the last seen mention
